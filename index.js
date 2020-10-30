2e191b7a6540eeab9349a761b9aeb6cd1bd31572
LZxLYt <71182225+LZxLYt@users.noreply.github.com>
2020-10-30 07:41:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://blacksakura.glitch.me"]
+const urls = ["https://l-zxl.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
